7bfadab491bcf74dc7c3f938062a6687eaf25336
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2025-08-01 16:15:33 -0300

    Refactor: Update dependencies and Kotlin configuration
    This commit updates several dependencies and adjusts the Kotlin configuration in the project.
    
    Key changes:
    - Updated Android Gradle Plugin (AGP) from `8.11.1` to `8.12.0`.
    - Updated JUnit version from `1.2.1` to `1.3.0`.
    - Updated Espresso Core from `3.6.1` to `3.7.0`.
    - Updated NDK version from `29.0.13113456 rc1` to `29.0.13846066 rc3`.
    - Modified `app/build.gradle.kts` to use `JvmTarget.JVM_17` for Kotlin compiler options.
    - Removed XML declaration from `.idea/misc.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -35,8 +37,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
     }
 
     buildFeatures {
@@ -44,7 +48,7 @@ android {
         viewBinding = true
     }
     buildToolsVersion = "36.0.0"
-    ndkVersion = "29.0.13113456 rc1"
+    ndkVersion = "29.0.13846066 rc3"
 }
 
 dependencies {
